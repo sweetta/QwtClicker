7e3924525973748615d151a6a7c589f4114015ba
Peter Mackenzie <mackheln@gmail.com>
2018-02-15 17:24:05 -0800

    Figured out selection process in Qwt
    This may not be the best solution, but it works.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -8,6 +8,8 @@
 
 #include "qwt_picker.h"
 #include "qwt_plot_picker.h"
+#include "qwt_plot_item.h"
+#include "qwt_plot_shapeitem.h"
 #include "qwt_picker_machine.h"
 
 #include <QDebug>
@@ -23,18 +25,41 @@ MainWindow::MainWindow(QWidget *parent) :
 
     plot->setCanvasBackground(Qt::white);
 
+    /* create a curve */
+
     curve = new QwtPlotCurve();
-    curve->setPen(QPen(QColor(255, 0, 0), 3));
+    curve->setPen(Qt::blue, 3);
 
     QPolygonF poly;
     poly << QPointF( 10, 10)
          << QPointF( 90, 10)
+         << QPointF( 50, 50)
          << QPointF( 90, 90)
-         << QPointF( 10, 90);
+         << QPointF( 10, 90)
+         << QPointF( 10, 10);
 
     curve->setSamples(poly);
 
     curve->attach(plot);
+
+    /* create a shapeItem */
+
+    shape = new QwtPlotShapeItem("star");
+
+    QPolygonF star;
+    star << QPointF( 20, 20)
+         << QPointF( 50, 40)
+         << QPointF( 80, 20)
+         << QPointF( 60, 50)
+         << QPointF( 80, 80)
+         << QPointF( 50, 60)
+         << QPointF( 20, 80)
+         << QPointF( 40, 50)
+         << QPointF( 20, 20);
+    shape->setPolygon(star);
+    shape->setBrush(Qt::yellow);
+    shape->setPen(Qt::darkYellow, 2);
+    shape->attach(plot);
 
     plot->setAxisScale(QwtPlot::yLeft, 0, 100);
     plot->setAxisScale(QwtPlot::xBottom, 0, 100);
@@ -59,6 +84,13 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::reset()
+{
+    curve->setPen(Qt::blue, 3);
+    shape->setBrush(Qt::yellow);
+    shape->setPen(Qt::darkYellow, 3);
+}
+
 void MainWindow::on_picker_activated (bool on)
 {
     qWarning() << "picker activated " << on;
@@ -71,19 +103,45 @@ void MainWindow::on_picker_selected (const QPolygon &polygon)
 
 void MainWindow::on_picker_appended (const QPoint &pos)
 {
+    qWarning() << "picker appended " << pos;
+
+    this->reset();
+
     double coords[ QwtPlot::axisCnt ];
     coords[ QwtPlot::xBottom ] = plot->canvasMap( QwtPlot::xBottom ).invTransform( pos.x() );
     coords[ QwtPlot::xTop ]    = plot->canvasMap( QwtPlot::xTop ).invTransform( pos.x() );
     coords[ QwtPlot::yLeft ]   = plot->canvasMap( QwtPlot::yLeft ).invTransform( pos.y() );
     coords[ QwtPlot::yRight ]  = plot->canvasMap( QwtPlot::yRight ).invTransform( pos.y() );
 
-    qWarning() << "picker appended " << pos
-               //<< QwtPlot::axisCnt
-               << coords[QwtPlot::xBottom]
-               //<< coords[QwtPlot::xTop]
-               << coords[QwtPlot::yLeft]
-               //<< coords[QwtPlot::yRight]
-               ;
+    QwtPlotItem *item = itemAt(pos);
+
+    if ( item )
+    {
+        if ( item->rtti() == QwtPlotItem::Rtti_PlotShape )
+        {
+            QwtPlotShapeItem *theShape = static_cast<QwtPlotShapeItem *>(item);
+            theShape->setPen(Qt::red, 2);
+            QBrush brush = theShape->brush();
+            QColor color = brush.color();
+            color.setAlpha(64);
+            brush.setColor(color);
+            theShape->setBrush(brush);
+        }
+
+        if ( item->rtti() == QwtPlotItem::Rtti_PlotCurve )
+        {
+            QwtPlotCurve *theCurve = static_cast<QwtPlotCurve *>(item);
+            curve->setPen(Qt::darkGreen);
+        }
+
+        plot->replot();
+
+        qWarning() << "item identified:" << item->rtti();
+    }
+    else
+    {
+        qWarning() << "no item itentified at" << coords[QwtPlot::xBottom] << coords[QwtPlot::yLeft];
+    }
 }
 
 void MainWindow::on_picker_moved (const QPoint &pos)
@@ -99,4 +157,91 @@ void MainWindow::on_picker_removed (const QPoint &pos)
 void MainWindow::on_picker_changed (const QPolygon &selection)
 {
     qWarning() << "picker changed " << selection;
+}
+
+QwtPlotItem* MainWindow::itemAt( const QPoint& pos ) const
+{
+    if ( plot == NULL )
+        return NULL;
+
+    // translate pos into the plot coordinates
+    double coords[ QwtPlot::axisCnt ];
+    coords[ QwtPlot::xBottom ] = plot->canvasMap( QwtPlot::xBottom ).invTransform( pos.x() );
+    coords[ QwtPlot::xTop ]    = plot->canvasMap( QwtPlot::xTop ).invTransform( pos.x() );
+    coords[ QwtPlot::yLeft ]   = plot->canvasMap( QwtPlot::yLeft ).invTransform( pos.y() );
+    coords[ QwtPlot::yRight ]  = plot->canvasMap( QwtPlot::yRight ).invTransform( pos.y() );
+
+    QwtPlotItemList items = plot->itemList();
+    for ( int i = items.size() - 1; i >= 0; i-- )
+    {
+        QwtPlotItem *item = items[ i ];
+        if ( item->isVisible() && item->rtti() == QwtPlotItem::Rtti_PlotCurve )
+        {
+            double dist;
+
+            QwtPlotCurve *curveItem = static_cast<QwtPlotCurve *>( item );
+            const QPointF p( coords[ item->xAxis() ], coords[ item->yAxis() ] );
+
+            if ( curveItem->boundingRect().contains( p ) || true )
+            {
+                // trace curves ...
+                dist = 1000.;
+                for (size_t line=0; line < curveItem->dataSize() - 1; line++)
+                {
+                    QPointF pnt;
+                    double x, y;
+
+                    pnt = curveItem->sample(line);
+                    x = plot->canvasMap( QwtPlot::xBottom ).transform( pnt.x() );
+                    y = plot->canvasMap( QwtPlot::yLeft ).transform( pnt.y() );
+                    QPointF x0(x,y);
+
+                    pnt = curveItem->sample(line+1);
+                    x = plot->canvasMap( QwtPlot::xBottom ).transform( pnt.x() );
+                    y = plot->canvasMap( QwtPlot::yLeft ).transform( pnt.y() );
+                    QPointF x1(x,y);
+
+                    QPointF r  = pos - x0;
+                    QPointF s  = x1 - x0;
+                    double s2  = QPointF::dotProduct(s,s);
+
+                    if (s2 > 1e-6)
+                    {
+                        double xi  = QPointF::dotProduct(r,s) / s2;
+
+                        if ( 0.0 <= xi && xi <= 1.0 )
+                        {
+                            QPointF t(-s.y()/sqrt(s2), s.x()/sqrt(s2));
+                            double d1 = QPointF::dotProduct(r,t);
+                            if ( d1 < 0.0 )  { d1 = -d1; }
+                            if ( d1 < dist ) { dist = d1;}
+                        }
+                    }
+                    else
+                    {
+                        dist = sqrt(QPointF::dotProduct(r,r));
+                        QPointF r2 = pos - x1;
+                        double d2  = sqrt(QPointF::dotProduct(r,r));
+                        if ( d2 < dist ) { dist = d2; }
+                    }
+                }
+
+                qWarning() << "curve dist =" << dist;
+
+                if ( dist <= 10 ) return static_cast<QwtPlotItem *>(curveItem);
+            }
+        }
+        if ( item->isVisible() && item->rtti() == QwtPlotItem::Rtti_PlotShape )
+        {
+            QwtPlotShapeItem *shapeItem = static_cast<QwtPlotShapeItem *>( item );
+            const QPointF p( coords[ item->xAxis() ], coords[ item->yAxis() ] );
+
+            if ( shapeItem->boundingRect().contains( p ) && shapeItem->shape().contains( p ) )
+            {
+                return static_cast<QwtPlotItem *>(shapeItem);
+            }
+        }
+    }
+
+    return NULL;
 }
